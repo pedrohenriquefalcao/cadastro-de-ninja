ba786d3fa21f10cfaa44611b772ec29d50857196
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-27 10:55:48 -0300

    primeiro commit

---
+++ cadastro-de-ninja/src/main/java/com/batismodejava/cadastro_de_ninja/CadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.batismodejava.cadastro_de_ninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+	}
+
+}
---
+++ cadastro-de-ninja/src/test/java/com/batismodejava/cadastro_de_ninja/CadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batismodejava.cadastro_de_ninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
