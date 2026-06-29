7d05ad0cd70f7de649ab1ecb30f9ac80c30150f1
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-28 23:57:11 -0300

    fix: movendo pra pasta em camada

--- cadastro-de-ninja/src/main/java/com/batismodejava/cadastro_de_ninja/Ninjas/NinjaController.java
+++ cadastro-de-ninja/src/main/java/com/batismodejava/cadastro_de_ninja/Ninjas/NinjaController.java
@@ -1,4 +1,4 @@
-package com.batismodejava.cadastro_de_ninja;
+package com.batismodejava.cadastro_de_ninja.Ninjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
