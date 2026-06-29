a45d60671b657183c1479b0417c24a9d9853695c
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-28 22:45:48 -0300

    feat: model Ninja

---
+++ cadastro-de-ninja/src/main/java/com/batismodejava/cadastro_de_ninja/NinjaModel.java
@@ -0,0 +1,40 @@
+package com.batismodejava.cadastro_de_ninja;
+
+public class NinjaModel {
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
