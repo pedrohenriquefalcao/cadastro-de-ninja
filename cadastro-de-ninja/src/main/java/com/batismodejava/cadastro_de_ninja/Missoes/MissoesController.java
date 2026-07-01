acb0c97d4fc0b3df9ee57145f58b804e2fe51a56
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-30 23:43:19 -0300

    fix: eliminado duplicata na rota

--- cadastro-de-ninja/src/main/java/com/batismodejava/cadastro_de_ninja/Missoes/MissoesController.java
+++ cadastro-de-ninja/src/main/java/com/batismodejava/cadastro_de_ninja/Missoes/MissoesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class MissoesController {
 
-@GetMapping("/boasVindas")
+@GetMapping("/boasVindasMissões")
     public String boasVindas() {
         return "Primeira mensagem da rota";
     }
