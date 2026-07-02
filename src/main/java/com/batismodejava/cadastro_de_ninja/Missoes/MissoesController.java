29adc6859295156ed4e3659340aae81aa79b823b
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-07-02 11:17:26 -0300

    feat: MISSOES > rotas criadas

--- src/main/java/com/batismodejava/cadastro_de_ninja/Missoes/MissoesController.java
+++ src/main/java/com/batismodejava/cadastro_de_ninja/Missoes/MissoesController.java
@@ -1,16 +1,34 @@
 package com.batismodejava.cadastro_de_ninja.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class MissoesController {
 
-@GetMapping("/boasVindasMissões")
-    public String boasVindas() {
+    @GetMapping("missoes/boasVindas")
+    public String boasVindasMissoes() {
         return "Primeira mensagem da rota";
+    }
+
+    @GetMapping("missoes/listar")
+    public String listarMissoes() {
+        return "Missões listadas com sucesso!";
+    }
+
+    @PostMapping("missoes/criar")
+    public String criarMissao() {
+        return "Missão criada!";
+    }
+
+    @PutMapping("missoes/alterar")
+    public String todasMissoes() {
+        return "Missão alterada!";
+    }
+
+    @DeleteMapping("missoes/excluir")
+    public String excluirMissao() {
+        return "Missão excluída!";
     }
 
 
